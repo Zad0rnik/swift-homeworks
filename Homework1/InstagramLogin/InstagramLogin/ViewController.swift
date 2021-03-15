75987322fdd21cd94154a83b5445d257a5507059
Zad0rnik <nikita.zadorozhnyi@nure.ua>
2021-03-15 23:40:22 +0200

    fixed some bugs and added some features

--- Homework1/InstagramLogin/InstagramLogin/ViewController.swift
+++ Homework1/InstagramLogin/InstagramLogin/ViewController.swift
@@ -13,23 +13,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        Email.layer.cornerRadius = 10
-        Password.layer.cornerRadius = 10
-        Login.layer.cornerRadius = 10
+        Email.layer.cornerRadius = 7
+        Password.layer.cornerRadius = 7
+        Login.layer.cornerRadius = 7
     }
     
     
-    //123
+    //Outlets
+    
+    
     @IBOutlet private var Email: UITextField!
     @IBOutlet private var Password: UITextField!
     @IBOutlet private var Login: UIButton!
-    @IBOutlet private var Hide: UIButton!
     @IBOutlet private var Facebook: UIButton!
+    @IBOutlet private var eyeButton: UIButton!
+    
+    
+    //Functions
+    
+    private var passwordTextIsHidden: Bool = true
+    
+    @IBAction func ShowPassword(_ sender: UIButton) {
+        print("Show Password")
+        passwordTextIsHidden = !passwordTextIsHidden
+        Password.isSecureTextEntry = passwordTextIsHidden
+    }
+    
+    @IBAction func ForgotPassword(_ sender: UIButton) {
+        print("Forgot Password")
+    }
+    @IBAction func LogIn(_ sender: UIButton) {
+        
+        print("Sign In")
+    }
+    
+    @IBAction func ContinueAs(_ sender: UIButton) {
+        print("Continue as Linuxoid")
+    }
+    
+    @IBAction func SignUp(_ sender: UIButton) {
+        print("Sign Up")
+    }
+    
+}
     
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         textField.resignFirstResponder()  //if desired
         return true
     }
     
-}
+
 
